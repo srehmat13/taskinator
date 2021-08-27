0714a300c1113ef47127a28cbb844232e93608bf
Shabia Harrington <shabsharrington@shabias-mbp.myfiosgateway.com>
2021-08-27 11:20:37 -0400

    save tasks to an array

--- module-4-assets/assets/js/script.js
+++ module-4-assets/assets/js/script.js
@@ -1,3 +1,5 @@
+var tasks = [];
+
 var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
@@ -5,6 +7,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content");
+
+
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -29,13 +33,16 @@ var taskFormHandler = function(event) {
     completeEditTask(taskNameInput, taskTypeInput, taskId);
   } else {
     var taskDataObj = {
-      name: taskNameInput,
-      type: taskTypeInput
+        name: taskNameInput,
+        type: taskTypeInput,
+        status: "to do"
+      }
     };
 
     createTaskEl(taskDataObj);
-  }
-};
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
+  };
 
 var createTaskEl = function(taskDataObj) {
   var listItemEl = document.createElement("li");
@@ -51,6 +58,10 @@ var createTaskEl = function(taskDataObj) {
   var taskActionsEl = createTaskActions(taskIdCounter);
   listItemEl.appendChild(taskActionsEl);
   tasksToDoEl.appendChild(listItemEl);
+
+taskDataObj.id = taskIdCounter;
+
+tasks.push(taskDataObj);
 
   // increase task counter for next unique id
   taskIdCounter++;
@@ -102,6 +113,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
   // set new values
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent = taskType;
+
+// loop through tasks array and task object with new content
+for (var i = 0; i < tasks.length; i++) {
+  if (tasks[i].id === parseInt(taskId)) {
+    tasks[i].name = taskName;
+    tasks[i].type = taskType;
+  }
+};
 
   alert("Task Updated!");
 
